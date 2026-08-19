5643fa02c42248635400e40459b3e92ab75fe3bc
LUCAS ANTONIO LINHAR <lucas_linhar@estudante.sesisenai.org.br>
2026-08-19 16:18:09 -0300

    67767676767

--- projetos/projeto_06/script.js
+++ projetos/projeto_06/script.js
@@ -9,6 +9,12 @@ btnEnviar.addEventListener("click", function(){
     adicionarTarefa()
 })
 
+inputText.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") {
+    adicionarTarefa()
+  }
+});
+
 function adicionarTarefa() {
     //trim serve pra não ter espaço das pontas
     let texto = inputText.value.trim()
@@ -16,6 +22,29 @@ function adicionarTarefa() {
     if(texto !== "" ){
         contadorId++
         tarefas.push({id: contadorId , tarefa: texto , concluido:false})
-        console.log(tarefas) 
+        inputText.value = ""
+        renderizar()
     }
+}
+
+function renderizar(){
+    lista.innerHTML = ""
+    tarefas.forEach(function(item){
+        let li = document.createElement("li")
+        li.textContent = item.tarefa 
+        lista.appendChild(li)
+
+        let btnExcluir = document.createElement("button")
+        btnExcluir.classList.add("btnExcluir")
+        btnExcluir.innerText = "X"
+        li.appendChild(btnExcluir)
+        btnExcluir.addEventListener("click",function(e){
+            e.stopPropagation()
+
+            tarefas = tarefas.filter(function(t){
+                return t.id !== item.id
+            })
+            renderizar()
+        })
+    })
 }
